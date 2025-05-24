241f07afe8838abdc160b6472a8df96e16bafc23
Mohsin Khatik <68121805+mohsin8421@users.noreply.github.com>
2025-05-24 18:02:32 +0530

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,40 @@
+(function (){
+ "use strict";
+ let item=document.querySelectorAll('.timeline li');
+ function isElementInViewport(el){
+    var rect=el.getBoundingClientRect();
+    return(
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+ }
+ function callbackFunc(){
+    for(let i=0; i< item.length; i++){
+        if(isElementInViewport(item[i])){
+            item[i].classList.add('in-view')
+        }
+    }
+ }
+ window.addEventListener('load', callbackFunc)
+ window.addEventListener('resize', callbackFunc)
+ window.addEventListener('scroll', callbackFunc)
+
+})()
+
+
+
+/* Demo purposes only */
+$(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
+
+
+
+// navbar
+
+
